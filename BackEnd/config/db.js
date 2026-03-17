969425e4219eda811cd57269a770e120fc6b9e5c
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-03-18 04:20:45 +0500

    fix incorrect mongodb env variable name

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGODB_URI, {
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
       bufferCommands: false,
     });
   }
